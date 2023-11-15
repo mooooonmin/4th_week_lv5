f8cdb5d2523305c457999ca86cf14f36aef50dbf
min_moon <minhee610@gmail.com>
2023-11-15 16:56:38 +0900

    secretkey 변경2

--- src/main/java/com/level5/basket/users/UserService.java
+++ src/main/java/com/level5/basket/users/UserService.java
@@ -23,8 +23,8 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
-    @Value("${jwt.secret.key}")
-    private String secretKey;
+    //@Value("${jwt.secret.key}")
+    private final String secretKey = "7Iqk7YyM66W07YOA7L2U65Sp7YG065+9U3ByaW5n6rCV7J2Y7Yqc7YSw7LWc7JuQ67mI7J6F64uI64ukLg==";
 
     // 회원가입
     @Transactional
